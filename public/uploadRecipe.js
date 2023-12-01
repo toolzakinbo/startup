5298ae974037a35c66fb16df1af67328a614672e
Tolulope Akinbo <112359159+toolzakinbo@users.noreply.github.com>
2023-12-01 12:26:29 -0700

    changes to uploadRecipe

--- public/uploadRecipe.js
+++ public/uploadRecipe.js
@@ -1,41 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
+  const form = document.querySelector("#recipeForm");
   const uploadStatus = document.getElementById("uploadStatus");
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
+    console.log("Form submitted");
 
     const recipeTitle = document.getElementById("recipeTitle").value;
+    const recipeDescription = document.getElementById("recipeDescription").value;
     const ingredients = document.getElementById("ingredients").value;
     const instructions = document.getElementById("instructions").value;
     const recipeImage = document.getElementById("recipeImage").files[0];
+    
 
-    if (!recipeTitle || !ingredients || !instructions || !recipeImage) {
+    if (!recipeTitle || !recipeDescription || !ingredients || !instructions) {
       uploadStatus.textContent = "Please fill in all fields and choose an image.";
       return;
     }
 
-    const formData = new FormData();
-    formData.append("title", recipeTitle); 
-    formData.append("ingredients", ingredients);
-    formData.append("instructions", instructions);
-    formData.append("image", recipeImage);
-
     try {
       const response = await fetch('/api/recipes', {
         method: 'POST',
-        body: formData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: recipeTitle,
+          description: recipeDescription,
+          ingredients: ingredients,
+          instructions: instructions,
+          image: recipeImage,
+        }),
       });
 
       if (response.status === 201) {
-        uploadStatus.textContent = "Recipe uploaded successfully!";
-        form.reset(); 
-        displayRecipes(); 
+        console.log("Recipe uploaded successfully")
+        //uploadStatus.textContent = "Recipe uploaded successfully!";
+        form.reset();
+        displayRecipes();
+        window.location.href = 'recreview.html';
       } else {
-        uploadStatus.textContent = "Error uploading the recipe.";
+        const errorMessage = await response.text();
+        uploadStatus.textContent = `Error uploading the recipe: ${errorMessage}`;
       }
     } catch (error) {
-      uploadStatus.textContent = "An error occurred while uploading the recipe.";
+      form.reset();
+      window.location.href = 'recreview.html';
     }
   });
 });
+
